edc190ca903b36c28cdec24dfb25d0eb776a9da5
Pavel Barchenko <Pavel.Barchenko@ibm.com>
2023-04-23 04:32:56 +0200

    update lib

--- src/main/java/com/core/im/modal/bucket/Bucket.java
+++ src/main/java/com/core/im/modal/bucket/Bucket.java
@@ -1,20 +1,18 @@
 package com.core.im.modal.bucket;
 
-import com.core.im.modal.order.Order;
 import com.core.im.modal.product.Product;
 import com.core.im.modal.user.AppUser;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import java.util.List;
 
--- src/main/java/com/core/im/modal/liked/Liked.java
+++ src/main/java/com/core/im/modal/liked/Liked.java
@@ -2,14 +2,14 @@ package com.core.im.modal.liked;
 
 import com.core.im.modal.product.Product;
 import com.core.im.modal.user.AppUser;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "liked")
